67979c55af8ffe451bbab01226b0603927f651b9
Abdurrehman <abdurrehman.wsb@gmail.com>
2024-10-14 21:57:43 +0500

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react'
+import { InputBox } from './components';
+import useCurrencyInfo from './hooks/useCurrencyInfo';
 import './App.css'
 // import './InputBox'
 
 function App() {
+
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("pkr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo =useCurrencyInfo(from)
 
   return (
     <>
